dcbfd229fbe9877eaad5ae970a48b3067c5165b0
OLGA KOLIKHALOVA <92117874+KOLIKOLGA@users.noreply.github.com>
2024-01-05 06:29:35 +0300

    first commit №2

---
+++ script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const isNumber = function (num) {
+  return !isNaN(parseFloat(num) && isFinite(num));
+};
+
+function startGame() {
+  let b = confirm("Угадай число от 1 до 100");
+  if (b) {
+    function one(min, max) {
+      let randomNum = Math.floor(Math.random() * (max - min + 1));
+
+      function two() {
+        let a = +prompt("Введите число");
+
+        function three() {
+          if (a > randomNum) {
+            a = +prompt("Ваше число больше загаданного. Введите другое число");
+
+            if (a) {
+              three();
+            } else {
+              alert("До свидания");
+            }
+          }
+          if (a < randomNum) {
+            a = +prompt("Ваше число меньше загаданного. Введите другое число");
+            if (a) {
+              three();
+            } else {
+              alert("До свидания");
+            }
+          }
+          if (a === randomNum) {
+            a = confirm("Вы угадали! Хотите повторить?");
+            if (a) {
+              one(1, 100);
+            } else {
+              alert("До свидания");
+              a = randomNum;
+            }
+          }
+        }
+        three();
+      }
+      two();
+    }
+    one(1, 100);
+  } else {
+    alert("До свидания");
+  }
+}
+startGame();
