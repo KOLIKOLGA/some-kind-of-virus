819905928890865d2445d92d09da02a274626468
OLGA KOLIKHALOVA <92117874+KOLIKOLGA@users.noreply.github.com>
2024-01-08 21:44:43 +0300

    rename function

--- script.js
+++ script.js
@@ -4,50 +4,40 @@ const isNumber = function (num) {
   return !isNaN(parseFloat(num) && isFinite(num));
 };
 
-function startGame() {
-  let b = confirm("Угадай число от 1 до 100");
-  if (b) {
-    function one(min, max) {
-      let randomNum = Math.floor(Math.random() * (max - min + 1));
+function startGame(min, max) {
+  let randomNum = Math.floor(Math.random() * (max - min + 1));
+  let suggestPlay = confirm("Угадай число от 1 до 100");
+  if (suggestPlay) {
+    function guessNumber() {
+      let inputNumber = prompt("Введите число");
 
-      function two() {
-        let a = prompt("Введите число");
-
-        function three() {
-          if (a > randomNum) {
-            a = prompt("Ваше число больше загаданного. Введите другое число");
-
-            if (a) {
-              three();
-            } else {
-              alert("До свидания");
-            }
+      if (inputNumber) {
+        if (isNumber(inputNumber)) {
+          if (inputNumber > randomNum) {
+            inputNumber = alert(
+              "Ваше число больше загаданного. Введите другое число"
+            );
+            guessNumber();
           }
-          if (a < randomNum) {
-            a = prompt("Ваше число меньше загаданного. Введите другое число");
-            if (a) {
-              three();
-            } else {
-              alert("До свидания");
-            }
+          if (inputNumber < randomNum) {
+            inputNumber = alert(
+              "Ваше число меньше загаданного. Введите другое число"
+            );
+            guessNumber();
           }
-          if (a == randomNum) {
-            a = confirm("Вы угадали! Хотите повторить?");
-            if (a) {
-              one(1, 100);
-            } else {
-              alert("До свидания");
-              a = randomNum;
-            }
+          if (inputNumber == randomNum) {
+            inputNumber = alert("Поздравляю, Вы угадали!!!");
           }
+        } else {
+          guessNumber();
         }
-        three();
+      } else {
+        alert("Игра окончена");
       }
-      two();
     }
-    one(1, 100);
+    guessNumber();
   } else {
-    alert("До свидания");
+    alert("Игра окончена");
   }
 }
-startGame();
+startGame(1, 100);
